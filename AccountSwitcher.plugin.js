dccaafd656ee243af07d30d933fda6293f68fe26
John Meow <mr.kofe.edk@ya.ru>
2019-07-18 13:35:13 +0300

    Removed if

--- AccountSwitcher.plugin.js
+++ AccountSwitcher.plugin.js
@@ -48,16 +48,14 @@ class AccountSwitcher {
 	}
 
 	loadStrings(){
-		if (!this.strings) {
-			var xmlHttp = new XMLHttpRequest();
-			xmlHttp.open("GET", 'https://l0c4lh057.github.io/AccountSwitcher/translations.json', false); // false for synchronous request
-	    		xmlHttp.send(null);
-			this.strings = JSON.parse(xmlHttp.responseText);
-		}
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open("GET", 'https://l0c4lh057.github.io/AccountSwitcher/translations.json', false); // false for synchronous request
+    	xmlHttp.send(null);
+		this.strings = JSON.parse(xmlHttp.responseText);
 	}
 
 	get local(){
-		loadStrings();
+		if (!this.strings) loadStrings();
 		return this.strings[this.lang] || this.strings["en"];
 	}
 
@@ -382,7 +380,7 @@ class AccountSwitcher {
 				NeatoLib.showToast(this.local.settings.copiedToken, "success");
 			}, "margin-top:10px;"), this.getName());
 			NeatoLib.Settings.pushElement(NeatoLib.Settings.Elements.createButton(this.local.settings.fetchTranslations, e => {
-				loadStrings();	
+				loadStrings();
 				NeatoLib.showToast(this.local.settings.fetchedTranslations, "success");
 			}, "margin-left:10px;margin-top:10px;"), this.getName());
 			NeatoLib.Settings.pushElement(NeatoLib.Settings.Elements.createButton(this.local.settings.support, e => {
